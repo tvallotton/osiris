1a1bd79c6644af1e00fd39b81910f561203b6993
Tomás Vallotton <tvallotton@uc.cl>
2023-09-15 23:29:39 +0000

    test: add coverage for multithreaded wakeup calls

--- src/task/waker.rs
+++ src/task/waker.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)]
 
-use crate::detach;
+use crate::runtime::current_unwrap;
 
 use super::SharedTask;
 use std::mem::{forget, size_of, transmute};
@@ -16,6 +16,44 @@ fn raw_waker(data: *const ()) -> RawWaker {
     RawWaker::new(data, &RAW_WAKER_VTABLE)
 }
 
+unsafe fn wake_by_ref(data: *const ()) {
+    // Safety: its the same as the input type
+    let task = unsafe { SharedTask::from_raw(data) };
+    let new = task.clone();
+    new.waker().wake();
+    forget(task);
+}
+
+unsafe fn wake(data: *const ()) {
+    // Safety: its the same as the input type
+    let task = SharedTask::from_raw(data);
+    let is_same_thread = task.thread_id() == std::thread::current().id();
+    if is_same_thread {
+        wake_local(task);
+    } else {
+        let rt = current_unwrap("wake");
+        rt.executor.clone().spawn(wake_multithread(task), rt, true);
+    }
+}
+
+unsafe fn wake_local(task: SharedTask) {
+    let executor = task.meta().rt.executor;
+    let mut queue = executor.queue.borrow_mut();
+    queue.push_back(task);
+}
+
+async unsafe fn wake_multithread(task: SharedTask) {
+    let sender = task.meta().rt.executor.sender.clone();
+    let waker = task.waker();
+    let mut buf = [0; size_of::<Waker>()];
+    buf.as_mut_ptr().cast::<Waker>().write(waker);
+    let result = sender.write_nonblock(&buf).await;
+    if let Err(err) = result {
+        let _: Waker = transmute(buf);
+        panic!("failed to wake task: {err}");
+    }
+}
+
 const RAW_WAKER_VTABLE: RawWakerVTable = {
     let clone = |data| {
         // Safety: its the same as the input type
@@ -25,40 +63,6 @@ const RAW_WAKER_VTABLE: RawWakerVTable = {
         raw_waker(new.into_ptr())
     };
 
-    let wake = |data| unsafe {
-        // Safety: its the same as the input type
-        let task = SharedTask::from_raw(data);
-
-        if task.thread_id() == std::thread::current().id() {
-            let executor = task.meta().rt.executor;
-            let mut queue = executor.queue.borrow_mut();
-            queue.push_back(task);
-            return;
-        }
-        let sender = task.meta().rt.executor.sender.clone();
-
-        detach(async move {
-            let waker = task.waker();
-            let mut buf = [0; size_of::<Waker>()];
-            buf.as_mut_ptr().cast::<Waker>().write(waker);
-            sender
-                .write_nonblock(&buf)
-                .await
-                .map_err(|err| {
-                    let _: Waker = transmute(buf);
-                    panic!("failed to wake task: {err}");
-                })
-                .ok();
-        });
-    };
-
-    let wake_by_ref = |data| {
-        // Safety: its the same as the input type
-        let task = unsafe { SharedTask::from_raw(data) };
-        let new = task.clone();
-        new.waker().wake();
-        forget(task);
-    };
     let drop = |data| {
         // Safety: its the same as the input type
         unsafe { SharedTask::from_raw(data) };
